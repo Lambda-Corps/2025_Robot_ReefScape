4254afa52ecbd97e6907c60e705bc39c1ff38bbd
David Frederick <davidafrederick.va.usa.earth@gmail.com>
2025-01-18 20:17:04 -0500

    Updated Elevator Code to use FX (Falcon) instead of SRX

--- elevator2.py
+++ elevator2.py
@@ -7,10 +7,27 @@ from phoenix5 import (
     Faults,
     LimitSwitchNormal,
 )
+
+#===========================================================
+from phoenix6.configs import (
+    TalonFXConfiguration,
+    TalonFXConfigurator,
+)
+
+from phoenix6 import hardware, controls
+from phoenix6.hardware.talon_fx import TalonFX
+from phoenix6.controls.follower import Follower
+from phoenix6.signals.spn_enums import InvertedValue, NeutralModeValue
+from phoenix6.controls import DutyCycleOut, VelocityVoltage
+from phoenix6.unmanaged import feed_enable
+from phoenix6.configs import TalonFXConfiguration
+from phoenix6.signals.spn_enums import (InvertedValue, NeutralModeValue, FeedbackSensorSourceValue)
+from phoenix6 import StatusCode
+
+#===========================================================
+
 from wpilib import SmartDashboard, AnalogInput, RobotBase, Timer
-
 import constants
-
 
 class ELEVATOR(Subsystem):
     ELEVATOR_TOP_LIMIT = 5000
@@ -20,55 +37,105 @@ class ELEVATOR(Subsystem):
     def __init__(self) -> None:
         super().__init__()
 
-        self._ELEVATOR: TalonSRX = TalonSRX(constants.ELEVATOR)
-        self._ELEVATOR.configFactoryDefault()
-        # self.ELEVATOR.configReverseLimitSwitchSource(
-        #     LimitSwitchSource.FeedbackConnector, LimitSwitchNormal.NormallyOpen, 0
-        # )
-        # self.ELEVATOR.configForwardSoftLimitThreshold(self.ELEVATOR_TOP_LIMIT)
-        # self.ELEVATOR.configForwardSoftLimitEnable(True)
-        self._ELEVATOR.setSensorPhase(True)
-        self._ELEVATOR.setSelectedSensorPosition(0)
+#===========================================================
+# DF 1-17-2026:   Changed from a Talon SRX to Talon FX initialization sequence
+        self._ELEVATOR: TalonFX = self.__configure_elevatorMotor()
+
+    def __configure_elevatorMotor(self,) -> TalonFX:
+        
+        # Applying a new configuration will erase all other config settings since
+        # we start with a blank config so each setting needs to be explicitly set
+        # here in the config method
+        talon = TalonFX(constants.ELEVATOR)
+
+        # Perform any other configuration
+        config: TalonFXConfiguration = TalonFXConfiguration()
+        config.slot0.k_s = 0.15  # Measured .15 volts to overcome static friction
+        config.slot0.k_v = 0.11  # Measured .19 for 1ps
+        config.slot0.k_a = 0.01  # complete guess, not measured
+        config.slot0.k_p = (
+            0.18  # an error of 1 RPS should add almost .19 more to correct
+        )
+        config.slot0.k_i = 0
+        config.slot0.k_d = 0
+
+        # Set the left side motors to be counter clockwise positive
+        # config.motor_output.inverted = InvertedValue.COUNTER_CLOCKWISE_POSITIVE
+        config.motor_output.inverted = InvertedValue.CLOCKWISE_POSITIVE
+
+        config.motor_output.neutral_mode = NeutralModeValue.BRAKE
+
+        # This configuration item supports counting wheel rotations
+        # This item sets the gear ratio between motor turns and wheel turns
+        config.feedback.feedback_sensor_source = FeedbackSensorSourceValue.ROTOR_SENSOR
+        config.feedback.sensor_to_mechanism_ratio = 10
+
+        # Apply the configuration to the motors
+        for i in range(6):  # Try 5 times with results returned into "ret"
+            ret = talon.configurator.apply(config)
+
+        talon.set_position(0)    #  Reset the encoder to  zero
+
+        return talon
+
+#===========================================================
+
+
+        # self._ELEVATOR: TalonSRX = TalonSRX(constants.ELEVATOR)
+        # self._ELEVATOR.configFactoryDefault()
+        # # self.ELEVATOR.configReverseLimitSwitchSource(
+        # #     LimitSwitchSource.FeedbackConnector, LimitSwitchNormal.NormallyOpen, 0
+        # # )
+        # # self.ELEVATOR.configForwardSoftLimitThreshold(self.ELEVATOR_TOP_LIMIT)
+        # # self.ELEVATOR.configForwardSoftLimitEnable(True)
+        # self._ELEVATOR.setSensorPhase(True)
+        # self._ELEVATOR.setSelectedSensorPosition(0)
 
         
-        # self._right_ELEVATOR.configReverseLimitSwitchSource(
-        #     LimitSwitchSource.FeedbackConnector, LimitSwitchNormal.NormallyOpen, 0
-        # )
-        # self._right_ELEVATOR.configForwardSoftLimitThreshold(self.ELEVATOR_TOP_LIMIT)
-        # self._right_ELEVATOR.configForwardSoftLimitEnable(True)
+        # # self._right_ELEVATOR.configReverseLimitSwitchSource(
+        # #     LimitSwitchSource.FeedbackConnector, LimitSwitchNormal.NormallyOpen, 0
+        # # )
+        # # self._right_ELEVATOR.configForwardSoftLimitThreshold(self.ELEVATOR_TOP_LIMIT)
+        # # self._right_ELEVATOR.configForwardSoftLimitEnable(True)
 
-        self._left_faults: Faults = Faults()
+        # self._left_faults: Faults = Faults()     <<<<  Not used in Talon FX
+    
     
 
     def move_ELEVATOR_up(self) -> None:
-        self._ELEVATOR.set(TalonSRXControlMode.PercentOutput, self.ELEVATOR_UP_SPEED)
-    
+        # self._ELEVATOR.set(TalonSRXControlMode.PercentOutput, self.ELEVATOR_UP_SPEED)  # SRX control code
+        self._ELEVATOR.set_control(controls.DutyCycleOut(self.ELEVATOR_UP_SPEED))  # FX Control Code
 
     def move_ELEVATOR_down(self) -> None:
-        self._ELEVATOR.set(
-            TalonSRXControlMode.PercentOutput, self.ELEVATOR_DOWN_SPEED
-        )
+        # self._ELEVATOR.set(TalonSRXControlMode.PercentOutput, self.ELEVATOR_DOWN_SPEED  # SRX control code
+        self._ELEVATOR.set_control(controls.DutyCycleOut(self.ELEVATOR_DOWN_SPEED))  # FX Control Code
+
 
     def ELEVATOR_at_top(self) -> bool:
-        return (
-            self._left_faults.ForwardLimitSwitch
-        )
+        # return (self._left_faults.ForwardLimitSwitch)    # SRX control code
+        atforwardLimit: bool = (self._ELEVATOR.get_forward_limit()==1)   # FX code
+        return atforwardLimit          # FX Control Code
+
+
+        # Notes on:  StatusSignal[ForwardLimitValue]
+        # https://api.ctr-electronics.com/phoenix6/release/python/autoapi/phoenix6/hardware/core/core_talon_fx/index.html
+
+        # https://v6.docs.ctr-electronics.com/en/stable/docs/api-reference/api-usage/actuator-limits.html
+        # https://api.ctr-electronics.com/phoenix6/release/java/com/ctre/phoenix6/hardware/core/CoreTalonFX.html
+    
 
     def stop_ELEVATOR_motors(self) -> None:
-        self._ELEVATOR.set(TalonSRXControlMode.PercentOutput, 0)
+        # self._ELEVATOR.set(TalonSRXControlMode.PercentOutput, 0)   # SRX control code
+        self._ELEVATOR.set_control(controls.DutyCycleOut(0))       # FX Control Code
         
     def periodic(self) -> None:
-        self._ELEVATOR.getFaults(self._left_faults)
+        # self._ELEVATOR.getFaults(self._left_faults)          # SRX Control code
        
-
-        SmartDashboard.putBoolean(
-            "L Forward Limit", self._left_faults.ForwardLimitSwitch
-        )
-        
-        
-        SmartDashboard.putBoolean(
-            "L Reverse Limit", self._ELEVATOR.isRevLimitSwitchClosed()
-        )
+        # SmartDashboard.putBoolean("EL Forward Limit", self._left_faults.ForwardLimitSwitch)  # SRX
+        SmartDashboard.putBoolean("Elev Forward Limit", self._ELEVATOR.get_forward_limit()==1)                                 
+                                          
+        # SmartDashboard.putBoolean("Elev Reverse Limit", self._ELEVATOR.isRevLimitSwitchClosed())
+        SmartDashboard.putBoolean("Elev Reverse Limit", (self._ELEVATOR.get_reverse_limit()==1) )
         
 
 
