d63ca15a059b7e3ded25aefa510d824d2636f02e
David Frederick <davidafrederick.va.usa.earth@gmail.com>
2025-02-25 12:45:07 -0500

    Created Sample Sequence Command Group

---
+++ multiSubsystemCommands.py
@@ -0,0 +1,19 @@
+import commands2
+from commands2 import PrintCommand 
+from elevator import ELEVATOR, MoveELEVATORToSetPoint
+from wrist import WristControl, Set_Global_Wrist_Angle
+import constants
+
+
+class TwoSubsystemCommandGroup(commands2.SequentialCommandGroup):
+   def __init__(self, _elevator: ELEVATOR, _wrist : WristControl) -> None:
+       super().__init__()
+       print ("Within SequentialCommand Group")
+
+       self._elevator = _elevator
+       self._wrist = _wrist
+       
+       self.addCommands(Set_Global_Wrist_Angle(self._wrist, 0))  
+       self.addCommands(MoveELEVATORToSetPoint(self._elevator,constants.ElevatorPosition.LEVEL_ONE).withTimeout(5))
+
+       self.addCommands(PrintCommand("TwoSubsystemCommandGroup Done"))
